d394709af74d6404bd389c485646d68fe30bb20a
Sam Duggan <samduggan@me.com>
2025-11-11 18:57:21 +0000

    type fix

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -9,7 +9,11 @@
 // }
 "use client";
 
-export default function MainHeader({text}) {
+type MainHeaderProp = {
+    text: string;
+}
+
+export default function MainHeader({text}: MainHeaderProp) {
     return (
         <h2 className="text-4xl sm:text-5xl font-bold mb-4 text-black">{text}</h2>
     );
